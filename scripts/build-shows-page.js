9b117a60cd39d81421a4d314f4a082f0e765177c
Charlotte <charlotteyuqiu@hotmail.com>
2024-05-26 16:15:50 -0400

    scripts completed

---
+++ scripts/build-shows-page.js
@@ -0,0 +1,112 @@
+// My array for shows
+const shows = [
+  {
+    timestamp: new Date("Mon Sept 09 2024").toLocaleString(),
+    venue: "Ronald Lane ",
+    location: "San Francisco, CA",
+  },
+
+  {
+    timestamp: new Date("Tue Sept 17 2024  ").toLocaleString(),
+    venue: "Pier 3 East ",
+    location: "San Francisco, CA",
+  },
+
+  {
+    timestamp: new Date("Sat Oct 12 2024  ").toLocaleString(),
+    venue: "View Lounge ",
+    location: "San Francisco, CA",
+  },
+
+  {
+    timestamp: new Date("Sat Nov 16 2024").toLocaleString(),
+    venue: "Hyatt Agency ",
+    location: "San Francisco, CA",
+  },
+
+  {
+    timestamp: new Date("Fri Nov 29 2024").toLocaleString(),
+    venue: "Moscow Center",
+    location: "San Francisco, CA",
+  },
+
+  {
+    timestamp: new Date("Wed Dec 18 2024 ").toLocaleString(),
+    venue: "Press Club",
+    location: "San Francisco, CA",
+  },
+];
+
+// Add shows section by DOM manipulation
+// create elements, add class, append elements for section, heading
+const section = document.createElement("section");
+section.classList.add("show");
+const container = document.createElement("div");
+container.classList.add("show__container");
+const heading = document.createElement("h2");
+heading.textContent = "Shows";
+heading.classList.add("show__heading");
+
+//create elements, add class, append elements for ticket details
+const displayDiv = document.createElement("div");
+displayDiv.classList.add("show--display");
+const list = document.createElement("ul");
+list.classList.add("show__list");
+
+// append elements
+displayDiv.appendChild(list);
+container.appendChild(heading);
+container.appendChild(displayDiv);
+section.appendChild(container);
+
+const footer = document.querySelector("footer");
+const parentOfFooter = footer.parentNode;
+parentOfFooter.insertBefore(section, footer);
+
+// iterate and display each show ticket
+const displayShows = (show) => {
+  const listItem = document.createElement("li");
+  listItem.classList.add("show__list-detail"); // Add a class for styling
+
+  // Create date
+  const dateTitle = document.createElement("p");
+  dateTitle.classList.add("show__sub-header");
+  dateTitle.textContent = `DATE`;
+  listItem.appendChild(dateTitle);
+  const dateInfo = document.createElement("p");
+  dateInfo.classList.add("show__date-detail");
+  dateInfo.textContent = `${show.timestamp}`;
+  listItem.appendChild(dateInfo);
+
+  // Create venue
+  const venueTitle = document.createElement("p");
+  venueTitle.classList.add("show__sub-header");
+  venueTitle.textContent = `VENUE`;
+  listItem.appendChild(venueTitle);
+  const venueInfo = document.createElement("p");
+  venueInfo.classList.add("show__list-each-detail");
+  venueInfo.textContent = `${show.venue}`;
+  listItem.appendChild(venueInfo);
+
+  // Create location
+  const locationTitle = document.createElement("p");
+  locationTitle.classList.add("show__sub-header");
+  locationTitle.textContent = `LOCATION`;
+  listItem.appendChild(locationTitle);
+  const locationInfo = document.createElement("p");
+  locationInfo.classList.add("show__list-each-detail");
+  locationInfo.textContent = `${show.location}`;
+  listItem.appendChild(locationInfo);
+
+  // Create the button
+  const button = document.createElement("button");
+  button.textContent = "BUY TICKETS";
+  button.classList.add("buy-tickets-button");
+  listItem.appendChild(button);
+
+  list.appendChild(listItem);
+};
+
+for (let i = 0; i < shows.length; i++) {
+  displayShows(shows[i]);
+}
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,13 +1,73 @@
-const conversationList = document.querySelector(".conversation__form");
+const commentList = document.querySelector(".conversation__list");
+// Initialize array of comments
+let comments = [
+  {
+    name: "Victor Pinto",
+    timestamp: new Date("2023-11-02").toLocaleString(),
+    comment:
+      "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
+  },
+  {
+    name: "Christina Cabrera",
+    timestamp: new Date("2023-10-28").toLocaleString(),
+    comment:
+      "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
+  },
+  {
+    name: "Isaac Tadesse",
+    timestamp: new Date("2023-10-20").toLocaleString(),
+    comment:
+      "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
+  },
+];
 
-form.addEventListener("comment", (event) => {
+// set class prototype
+class Comment {
+  constructor(name, timestamp, comment) {
+    this.name = name;
+    this.timestamp = timestamp;
+    this.comment = comment;
+  }
+}
+
+// li structure
+function addComment(comment) {
+  const listItem = document.createElement("li");
+  const nameSpan = document.createElement("span");
+  const timeSpan = document.createElement("span");
+  const commentText = document.createElement("p");
+
+  nameSpan.textContent = comment.name + " ";
+  timeSpan.textContent = ` [${comment.timestamp}] `;
+  commentText.textContent = comment.comment;
+
+  listItem.appendChild(nameSpan);
+  listItem.appendChild(timeSpan);
+  listItem.appendChild(commentText);
+  commentList.prepend(listItem);
+}
+
+// Add/render comments
+function renderComments() {
+  for (let i = comments.length - 1; i >= 0; i--) {
+    addComment(comments[i]);
+  }
+}
+
+// submit event
+const form = document.querySelector(".conversation__form");
+form.addEventListener("submit", (event) => {
   event.preventDefault();
   const name = event.target.name.value;
   const comment = event.target.comment.value;
 
   if (name !== "" && comment !== "") {
-    const newComment = new Comment(name, comment);
-    console.log(newComment);
+    const timestamp = new Date().toLocaleString();
+    const newComment = new Comment(name, timestamp, comment);
+    comments.push(newComment);
+    addCommentToDOM(newComment);
   }
   form.reset();
 });
+
+renderComments();
